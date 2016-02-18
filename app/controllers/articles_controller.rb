ca5019b4e0d5b3588624f21776cba8079361b09b
Anthony <anthonydevenuto@gmail.com>
2016-02-18 10:01:52 -0600

    added bootstrap

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,7 +16,7 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
     # renders in application.html.erb
     if @article.save
-      flash[:notice] = "Article was successfully created"
+      flash[:success] = "Article was successfully created"
       redirect_to article_path(@article)
     else
       render 'new'
