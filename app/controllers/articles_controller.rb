bc24bc929b6046ed85c155d6efb5645d979b511d
Anthony <anthonydevenuto@gmail.com>
2016-02-23 20:09:30 -0600

    added shared folder for errors/removed unneeded code

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -25,7 +25,7 @@ class ArticlesController < ApplicationController
 
   def update
     if @article.update(article_params)
-      flash[:notice] = "Article was successfully updated"
+      flash[:success] = "Article was successfully updated"
       redirect_to article_path(@article)
     else
       render 'edit'
@@ -39,7 +39,7 @@ class ArticlesController < ApplicationController
   def destroy
     @article.destroy
     redirect_to articles_path
-    flash[:notice] = "Article was successfully deleted"
+    flash[:success] = "Article was successfully deleted"
   end
 
   private
