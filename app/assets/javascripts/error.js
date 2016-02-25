15a2be3e3576126569144676960ddf4f4133ce27
Anthony <anthonydevenuto@gmail.com>
2016-02-25 09:59:45 -0600

    created user table/model

--- app/assets/javascripts/error.js
+++
@@ -1,8 +0,0 @@
-
-  function trigger(){
-    var article = $("#tError").data('object');
-    console.log(article)
-    $(".title").attr("placeholder", "<%= sanitize @article.errors[:title].first %>");
-    $(".title").attr("value", null);
-  };
-
