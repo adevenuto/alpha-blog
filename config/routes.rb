b65924e662a20dd330d21fce14979c0517b6ac86
Anthony <anthonydevenuto@gmail.com>
2016-02-26 17:02:29 -0600

    added user sign-up form

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'pages#index'
   get 'about', to: 'pages#about'
+  get 'signup', to: 'users#new'
 
+  resources :users, except: [:new]
   resources :articles
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
